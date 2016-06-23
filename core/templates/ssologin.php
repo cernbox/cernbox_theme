54c5ab90e2ef6866a6532bac1a9f6d089fdcdbe8
NadirRoGue <nadir.ro.gue@gmail.com>
2016-06-23 14:34:13 +0200

    Adding index2.php to allow our custom log in procedure

---
+++ core/templates/ssologin.php
@@ -0,0 +1,29 @@
+<?php /** @var $l OC_L10N */ ?>
+<?php
+vendor_script('jsTimezoneDetect/jstz');
+script('core', [
+	'visitortimezone',
+	'lostpassword',
+	'login'
+]);
+?>
+
+<!--[if IE 8]><style>input[type="checkbox"]{padding:0;}</style><![endif]-->
+<form method="post" name="login" action="/index2.php">
+	<fieldset>
+	<div id="message" class="hidden">
+		<img class="float-spinner" alt=""
+			src="<?php p(\OCP\Util::imagePath('core', 'loading-dark.gif'));?>">
+		<span id="messageText"></span>
+		<!-- the following div ensures that the spinner is always inside the #message div -->
+		<div style="clear: both;"></div>
+	</div>
+	<p class="grouptop">
+		<input type="submit" id="submit" title="<?php p($l->t('Proceed to CERNBox')); ?>" value="Proceed to CERNBox" disabled="disabled"/>
+	</p>
+
+	<input type="hidden" name="timezone-offset" id="timezone-offset"/>
+	<input type="hidden" name="timezone" id="timezone"/>
+	<input type="hidden" name="requesttoken" value="<?php /*p($_['requesttoken']) */?>">
+	</fieldset>
+</form>
